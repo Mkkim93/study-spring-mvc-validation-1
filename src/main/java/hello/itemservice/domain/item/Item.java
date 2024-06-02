bf2abb67db01c54c9a824fa3b04cba56a2a3ca3a
mkkim <king0031443@gmail.com>
2024-06-02 22:43:46 +0900

    # feat : study spring mvc validated

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,15 +1,25 @@
 package hello.itemservice.domain.item;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
     private Long id;
-    private String itemName;
-    private Integer price;
-    private Integer quantity;
 
+    @NotBlank // message 별도 지정 가능
+    private String itemName;
+
+    @NotNull // message 별도 지정 가능
+    @Range(min = 1000, max = 1000000) // message 별도 지정 가능
+    private Integer price;
+
+    @NotNull
+    @Range(max = 9999)
+    private Integer quantity;
 
     public Item() {
     }
--- src/main/java/hello/itemservice/web/validation/ItemValidator.java
+++ src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -1,17 +1,15 @@
 package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
-import hello.itemservice.domain.item.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 @Slf4j
-@Component
+@Component // 스프링 빈에 등록
 public class ItemValidator implements Validator {
 
 
@@ -21,16 +19,12 @@ public class ItemValidator implements Validator {
     }
 
     @Override
-    public void validate(Object target, Errors errors) { // Item 을 객체로 사용, BindingResult 의 부모 클래스 Errors 를 사용
+    public void validate(Object target, Errors errors) { // target 을 Item 객체로 다운캐스팅, BindingResult 의 부모 클래스인 Errors 를 사용
         Item item = (Item) target;
         log.info("objectName={}", errors.getObjectName()); // ObjectName = item
         log.info("target={}", errors.getClass()); // Target = Item(id, itemName, price, quantity)
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemName", "required");
-
-        if (item.getItemName().contains(" ")) {
-            errors.rejectValue("itemName", "notEmpty");
-        }
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
 
